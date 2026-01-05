542ea296595baec3d94f3ede20f8019da06deba0
Priit559 <priitkask559@gmail.com>
2026-01-05 20:31:10 +0200

    Revert "HealthTrackerApp"
    This reverts commit 91e921ef9b132362180e858253b699e52f380aa3.

--- HealthTrackerApp/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HealthTrackerApp")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HealthTrackerApp.Pages")]
